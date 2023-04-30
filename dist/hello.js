ee7718dee011807b728ceeefaa9f65044812b917
Amit Verma <amit.verma@opsio.se>
2023-04-30 13:42:34 +0530

    first commit

---
+++ dist/hello.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hello = void 0;
+class Hello {
+    constructor() {
+        this.helloMoto = () => {
+            console.log("Hello");
+        };
+        this.header = () => {
+            console.log("header");
+        };
+    }
+}
+exports.Hello = Hello;
+//# sourceMappingURL=hello.js.map
---
+++ src/hello.ts
@@ -0,0 +1,11 @@
+export class Hello{
+    public helloMoto() {
+        console.log('Method not implemented.')
+    }
+    constructor(){ }
+
+    header = () =>{
+        console.log("header")
+    }
+
+}
